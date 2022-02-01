2e4cd65f1dbafe835afc9ae62023abe222ebdfbc
veternumCattus <sleepyc.d@gmail.com>
2022-02-01 21:28:30 +0300

    start commit

---
+++ app/src/main/java/ru/geekbrains/course1/homework_2_layoutforcalculator/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.course1.homework_2_layoutforcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
